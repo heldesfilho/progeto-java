60a84a8a89a02ad9f0fca3ed92042d4f189449b4
heldesfilho <heldesfilho08@gmail.com>
2026-08-20 14:00:41 -0300

    Update listaDeTarefas.java

--- listaDeTarefas.java
+++ listaDeTarefas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class lista {
+public class listaDeTarefas {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         ArrayList<Tarefa> lista=new ArrayList<>();
