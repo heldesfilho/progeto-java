b204a4bd0cfe95899605b28e4b5c328f8beefe96
heldesfilho <heldesfilho08@gmail.com>
2026-08-20 14:06:23 -0300

    organização

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # progeto java
-repositorio dos progetos em java
+progetos em java
--- calculadoraSimples.java
+++ calculadoraSimples.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class calculadora {
+public class calculadoraSimples {
     public static void main(String[] args) {
         Scanner entrada =new Scanner(System.in);
         System.out.println("qual o primeiro número: ");
